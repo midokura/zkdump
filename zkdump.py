11e4299a5d4b8a737bec989f7eeeec289a8e30a3
Dave Cahill <dcahill@midokura.com>
2013-05-02 08:24:54 +0000

    Make it harder to load data into ZK, as this is destructive

--- zkdump.py
+++ zkdump.py
@@ -3,38 +3,13 @@ import sys
 import zc.zk
 from optparse import OptionParser
 
-# from http://stackoverflow.com/questions/3041986/python-command-line-yes-no-input
-def query_yes_no(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
+def confirm_destructive_action(question):
+    prompt = "\n[type confirm to proceed, any other key to cancel] "
 
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
+    sys.stdout.write(question + prompt)
+    choice = raw_input().lower()
 
-    The "answer" return value is one of "yes" or "no".
-    """
-    valid = {"yes":True,   "y":True,  "ye":True,
-             "no":False,     "n":False}
-    if default == None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "\
-                             "(or 'y' or 'n').\n")
+    return choice == "confirm"
 
 def get_zookeeper(options, args):
   zk = None
@@ -57,7 +32,7 @@ def import_zk(zk, filename):
   f = open(filename,"r")
 
   current_contents = ", ".join(zk.get_children("/"))
-  go_ahead = query_yes_no("Zookeeper currently contains data, with these root nodes: %s\nAre you sure you want to load file contents into Zookeeper? " % current_contents, None)
+  go_ahead = confirm_destructive_action("Zookeeper currently contains data, with these root nodes: %s\nAre you sure you want to load file contents into Zookeeper? " % current_contents)
 
   if go_ahead:
     print "Loading data..."
