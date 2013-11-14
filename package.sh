435c24df428abc484582a8a5102c11531522d3c2
Dave Cahill <dcahill@midokura.com>
2013-11-14 12:49:30 +0900

    Switch packaging to use FPM, add RPM packaging

--- package.sh
+++ package.sh
@@ -1,7 +1,25 @@
-# move file
-rm -f packaging/usr/bin/*
-cp src/* packaging/usr/bin/
+# Get version number from command line
+pkgver=$1
+if [ '$pkgver' == '' ]
+then
+	echo "Please specify the package version, e.g. 'package.sh 1.04' to package as version 1.04"
+	exit
+else
+    echo "Packaging with version number $pkgver"
+fi
 
-# package
-dpkg-deb --build packaging .
+# Common args for rpm and deb
+FPM_BASE_ARGS=$(cat <<EOF
+--name 'zkdump' --architecture 'noarch' --license '2013, Midokura' \
+--vendor 'Midokura' --maintainer 'Midokura' --url 'http://midokura.com' \
+--description 'Simple zookeeper dump and load script' \
+-d 'python' -d 'python-zookeeper' -d 'python-simplejson' \
+-s dir -C src --prefix /usr/bin --version $pkgver
+EOF
+)
 
+# package rpm
+eval fpm $FPM_BASE_ARGS --epoch 1 -t rpm .
+
+# package debian
+eval fpm $FPM_BASE_ARGS --deb-priority 'optional' -t deb .
