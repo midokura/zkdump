54caea9784f0f5dd99b083680c379749606578a9
Takayuki Usui <takayuki@midokura.com>
2013-11-29 17:15:37 +0900

    Use zkpython in RPM instead of python-zookeeper

--- package.sh
+++ package.sh
@@ -13,13 +13,13 @@ FPM_BASE_ARGS=$(cat <<EOF
 --name 'zkdump' --architecture 'noarch' --license '2013, Midokura' \
 --vendor 'Midokura' --maintainer 'Midokura' --url 'http://midokura.com' \
 --description 'Simple zookeeper dump and load script' \
--d 'python' -d 'python-zookeeper' -d 'python-simplejson' \
+-d 'python' -d 'python-simplejson' \
 -s dir -C src --prefix /usr/bin --version $pkgver
 EOF
 )
 
 # package rpm
-eval fpm $FPM_BASE_ARGS --epoch 1 -t rpm .
+eval fpm $FPM_BASE_ARGS -d 'zkpython' --epoch 1 -t rpm .
 
 # package debian
-eval fpm $FPM_BASE_ARGS --deb-priority 'optional' -t deb .
+eval fpm $FPM_BASE_ARGS -d 'python-zookeeper' --deb-priority 'optional' -t deb .
